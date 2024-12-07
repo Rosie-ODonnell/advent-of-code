e11d0958bca8a546a4dcc71d3861fe9df84db1a7
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-07 14:02:52 +0000

    remove comments and mark as WIP

--- 2024/day-two/task-two.py
+++ 2024/day-two/task-two.py
@@ -26,18 +26,6 @@ def check_for_bad_level(row, number_of_safe_reports, number_of_bad_levels_in_row
         if number_of_safe_levels == len(row):
             number_of_safe_reports += 1
 
-
-    # # if row == sorted(row) or row == sorted(row, reverse=True):
-    #     differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
-
-    #     if all([num in [1,2,3] for num in differences]):
-    #         number_of_safe_reports += 1
-    #     else:
-    #         number_of_bad_levels_in_row += 1
-
-    # else:
-    #         number_of_bad_levels_in_row += 1
-
     return number_of_safe_reports, number_of_bad_levels_in_row
 
 
@@ -73,47 +61,3 @@ print(check_number_of_safe_reports(structured_input))
  
 
 
-
-
-
-
-
-    # new_row = row if index_to_skip is None else row[:index_to_skip] + row[index_to_skip+1:]
-
-    # if new_row[0] > new_row[1]:
-    #     new_row.reverse()
-    
-    # for index in range(1, len(row)):    
-    #     print(abs(new_row[index]-new_row[index-1]), "abs", new_row, "new row")
-
-    #     if new_row[index] <= new_row[index -1] or abs(new_row[index] - new_row[index-1]) == 0 or abs(new_row[index]-new_row[index-1]) > 3:
-    #         check_for_bad_rows(number_of_safe_reports, new_row, index)
-    #         #if you call this once and it fails then this is an unsafe report because you can only remove one item in row
-        
-    #     else:
-    #         number_of_safe_reports += 1
-
-
-
-
-
-
-
-#  if row[0] > row[1]:
-#             row.reverse()
-
-#         for index in range(1, len(row)):
-#             number_of_bad_levels = 0
-
-#             if row[index] <= row[index -1]:
-#                 number_of_bad_levels += 1
-
-#             if abs(row[index] - row[index-1]) == 0 or abs(row[index]-row[index-1]) > 3:
-#                 print(row, abs(row[index] - row[index-1]))
-#                 number_of_bad_levels += 1
-
-
-#             if number_of_bad_levels >= 1:
-#                 print(number_of_bad_levels, "number_of_bad_levels")   
-#                 number_of_unsafe_reports += 1
-        
