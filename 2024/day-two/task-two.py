98b96a727c8bf3ac811b2c6ba9710fdd829de687
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-07 12:22:08 +0000

    swap in for loop to previous working solution

--- 2024/day-two/task-two.py
+++ 2024/day-two/task-two.py
@@ -12,20 +12,39 @@ from test_input import test_input
 
 
 
-def check_for_bad_level(row, number_of_safe_reports):
-    if row == sorted(row) or row == sorted(row, reverse=True):
-        differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
+def check_for_bad_level(row, number_of_safe_reports, number_of_bad_levels_in_row):
+    if row[0] > row[1]:
+        row.reverse()
+    
+    number_of_safe_levels = 1
 
-        if all([num in [1,2,3] for num in differences]):
-            # // need to enter recursion - need to loop again but this time remove one item in list
+    for index in range(0, len(row)): 
+        if row[index] > row[index -1]:
+            if not abs(row[index] - row[index-1]) == 0 and not abs(row[index]-row[index-1]) > 3:
+                number_of_safe_levels += 1
+
+        if number_of_safe_levels == len(row):
             number_of_safe_reports += 1
-            
-    return number_of_safe_reports
+
+
+    # # if row == sorted(row) or row == sorted(row, reverse=True):
+    #     differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
+
+    #     if all([num in [1,2,3] for num in differences]):
+    #         number_of_safe_reports += 1
+    #     else:
+    #         number_of_bad_levels_in_row += 1
+
+    # else:
+    #         number_of_bad_levels_in_row += 1
+
+    return number_of_safe_reports, number_of_bad_levels_in_row
+
 
 def check_for_bad_rows(number_of_safe_reports, reports):
-
     for row in reports:
-        number_of_safe_reports = check_for_bad_level(row, number_of_safe_reports)
+        number_of_bad_levels_in_row = 0
+        number_of_safe_reports, number_of_bad_levels_in_row = check_for_bad_level(row, number_of_safe_reports, number_of_bad_levels_in_row)
 
     return number_of_safe_reports
 
