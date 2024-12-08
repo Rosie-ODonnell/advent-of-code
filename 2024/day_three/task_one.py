0545fc8fc6a6b7c1743e4d8fc9182a5e5ce99ebc
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-08 20:32:49 +0000

    refactor

--- 2024/day_three/task_one.py
+++ 2024/day_three/task_one.py
@@ -1,21 +1,16 @@
 import re
 from input import input
-from test_input import test_input
 
 def find_mull_instructions(scrambled_instructions: str):
     multiply_regex = 'mul\(\d{1,3},\d{1,3}\)'
-
-    matches = re.findall(multiply_regex, scrambled_instructions)
+    mul_statements = re.findall(multiply_regex, scrambled_instructions)
     total = 0
 
-    for match in matches:
-        splitted = match.replace("mul(","").split(",")
-        multiplication = int(splitted[0]) * int(splitted[1][:-1])
-        total += multiplication
+    for statement in mul_statements:
+        splitted = statement.replace("mul(","").replace(")","").split(",")
+        total += int(splitted[0]) * int(splitted[1])
 
     return total
-
-
 
 print(find_mull_instructions(input))
 
