3a13e387b24619a6d8a67c0fa06a1f7983ae8fb5
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-09 20:09:52 +0000

    row is safe

--- 2024/2/task_two.py
+++ 2024/2/task_two.py
@@ -10,17 +10,22 @@ from test_input import test_input
 
 from input import input
 
+
+def row_is_safe(row): 
+    if row == sorted(row) or row == sorted(row, reverse=True):
+            differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
+
+            if all([num in [1,2,3] for num in differences]):
+                return True
+    
+    return False
+
 def check_number_of_safe_reports(reports):
     number_of_safe_reports = 0
 
     for row in reports:
-
-        if row == sorted(row) or row == sorted(row, reverse=True):
-            
-            differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
-
-            if all([num in [1,2,3] for num in differences]):
-                number_of_safe_reports += 1
+        if row_is_safe(row):
+            number_of_safe_reports += 1
         
     return number_of_safe_reports    
 
