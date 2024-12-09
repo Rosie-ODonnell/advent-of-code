2d6d54685965228cacfa6ea9e16682e311d2d3fe
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-09 19:34:45 +0000

    keep count of unsafe levels just by increasing or decreasing

--- 2024/2/task_two.py
+++ 2024/2/task_two.py
@@ -1,42 +1,6 @@
 from input import input
 from test_input import test_input
 
-# UNSOLVED - WIP
-
-
-def check_for_bad_level(row, number_of_safe_reports):
-    if row[0] > row[1]:
-        row.reverse()
-    
-    number_of_safe_levels = 1
-
-    for index in range(0, len(row)): 
-        if row[index] > row[index -1]:
-            if not abs(row[index] - row[index-1]) == 0 and not abs(row[index]-row[index-1]) > 3:
-                number_of_safe_levels += 1
-
-        if number_of_safe_levels == len(row):
-            number_of_safe_reports += 1
-
-    return number_of_safe_reports
-
-
-def check_number_of_safe_reports(reports):
-    number_of_safe_reports = 0
-    
-    for row in reports:
-        number_of_bad_levels_in_row = 0
-        number_of_safe_reports = check_for_bad_level(row, number_of_safe_reports)
-
-    return number_of_safe_reports
-        
-
-
-structured_input = [[int(num) for num in line.split()] for line in input.strip().split('\n')]
-print(check_number_of_safe_reports(structured_input))
-
-
-
 # 7 6 4 2 1: Safe without removing any level.
 # 1 2 7 8 9: Unsafe regardless of which level is removed.
 # 9 7 6 2 1: Unsafe regardless of which level is removed.
@@ -44,7 +8,51 @@ print(check_number_of_safe_reports(structured_input))
 # 8 6 4 4 1: Safe by removing the third level, 4.
 # 1 3 6 7 9: Safe without removing any level.
 
+def check_number_of_unsafe_levels(row):
+    number_of_unsafe_levels = 0
+
+    for index in range(1, len(row)):
+        if 0 == abs(row[index] - row[index-1]) or abs(row[index] - row[index-1]) > 3:
+            number_of_unsafe_levels += 1
+
+    return number_of_unsafe_levels
+
+
+def check_row(reports):
+    number_of_safe_reports = 0
+
+    for row in reports:
+        number_of_unsafe_levels = check_number_of_unsafe_levels(row)
+        if number_of_unsafe_levels < 1:
+            number_of_safe_reports += 1
+    
+    return number_of_safe_reports
+
+
+def check_number_of_safe_reports(reports):
+    number_of_safe_reports = check_row(reports)
+    return number_of_safe_reports
+        
+
+
+structured_input = [[int(num) for num in line.split()] for line in test_input.strip().split('\n')]
+print(check_number_of_safe_reports(structured_input))
 
  
 
 
+
+
+
+# if row[0] > row[1]:
+#         row.reverse()
+    
+#     number_of_safe_levels = 1
+
+#     for index in range(0, len(row)): 
+#         if row[index] > row[index -1]:
+#             if not abs(row[index] - row[index-1]) == 0 and not abs(row[index]-row[index-1]) > 3:
+#                 number_of_safe_levels += 1
+
+#         if number_of_safe_levels == len(row):
+#             number_of_safe_reports += 1
