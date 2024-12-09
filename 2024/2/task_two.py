d1654a2cd9e6b989ea9b1a0cacaa22cd70bfe0dc
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-09 20:08:03 +0000

    dont filter out to simplify for next part

--- 2024/2/task_two.py
+++ 2024/2/task_two.py
@@ -8,37 +8,32 @@ from test_input import test_input
 # 8 6 4 4 1: Safe by removing the third level, 4.
 # 1 3 6 7 9: Safe without removing any level.
 
-def check_number_of_unsafe_levels(row):
-    number_of_unsafe_levels = 0
+from input import input
 
-    for index in range(1, len(row)):
-        if not 0 < abs(row[index] - row[index-1]) < 4:
-            number_of_unsafe_levels += 1
-
-    return number_of_unsafe_levels
-
-
-def check_row(reports):
+def check_number_of_safe_reports(reports):
     number_of_safe_reports = 0
 
     for row in reports:
-        number_of_unsafe_levels = check_number_of_unsafe_levels(row)
-        if number_of_unsafe_levels < 1:
-            number_of_safe_reports += 1
-    
-    return number_of_safe_reports
 
+        if row == sorted(row) or row == sorted(row, reverse=True):
+            
+            differences = [abs(current-following) for current, following in zip(row[:-1], row[1:])]
 
-def check_number_of_safe_reports(reports):
-    number_of_safe_reports = check_row(reports)
-    return number_of_safe_reports
+            if all([num in [1,2,3] for num in differences]):
+                number_of_safe_reports += 1
         
+    return number_of_safe_reports    
 
 
-structured_input = [[int(num) for num in line.split()] for line in test_input.strip().split('\n')]
+structured_input = [[int(num) for num in line.split()] for line in input.strip().split('\n')]
 print(check_number_of_safe_reports(structured_input))
 
- 
+
+
+
+
+
+
 
 
 
