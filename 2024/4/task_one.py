e59faf8db983300c7c9999f42d8be2130df66388
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-10 19:21:38 +0000

    get count for up and down

--- 2024/4/task_one.py
+++ 2024/4/task_one.py
@@ -6,8 +6,23 @@ def find_xmas():
     
     xmas_count = 0
 
+    word_search_columns = {}
+
+    diagonal_lines = {}
+
     for row in word_search_grid:
         xmas_count += check_forwards_and_backwards(row)
+        for index, letter in enumerate(row):
+            if word_search_columns.get(index):
+                word_search_columns[index].append(letter)
+            else:
+                word_search_columns[index] = [letter]
+
+    for row in word_search_columns.values():
+        xmas_count += check_forwards_and_backwards(row)
+    
+
+    print(word_search_columns, "columns")
 
     # need to loop through each array comparing indexes
     # have separate funciton for each direction - check_forwards_and_backwards, check_up_and_down, check_diagonal, check_backwards
