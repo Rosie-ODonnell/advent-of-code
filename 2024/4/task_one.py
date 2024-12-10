2d7c6d42299c9f7d7bcaa39c4d697dd6cc89da48
Rosie O'Donnell <rosie.odonnell@talk21.com>
2024-12-10 19:08:49 +0000

    check_across_and_back

--- 2024/4/task_one.py
+++ 2024/4/task_one.py
@@ -1,14 +1,21 @@
+import re
+
 def find_xmas():
     with open("/Users/rosie/Documents/Code/advent-of-code/2024/4/test_input.txt") as word_search:
         word_search_grid = [[letters for letters in line] for line in word_search.read().strip().split('\n')]
+    
+    xmas_count = 0
 
+    for row in word_search_grid:
+        xmas_count += check_forwards_and_backwards(row)
 
     # need to loop through each array comparing indexes
     # have separate funciton for each direction - check_forwards_and_backwards, check_up_and_down, check_diagonal, check_backwards
+    return xmas_count
 
-    print(word_search_grid, "grid")
 
-
+def check_forwards_and_backwards(row):
+    return len(re.findall('XMAS', ''.join(row))+re.findall('XMAS', ''.join(row[::-1])))
 
 
 print(find_xmas())
